2fdf800621d289a20d3413f5af8fe2be207ef8d1
dagargo <dagargo@gmail.com>
2018-11-30 20:36:31 +0100

    Increase version to 1.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='MicroDude',
-    version='1.3',
+    version='1.3.1',
     description='Editor for Arturia MicroBrute',
     author='David García Goñi',
     author_email='dagargo@gmail.com',
