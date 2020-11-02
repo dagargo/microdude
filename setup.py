afcf8eeea582ef494928d4fc06c00e7192a7e4b2
dagargo <dagargo@gmail.com>
2020-11-02 13:04:57 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='MicroDude',
-    version='2.0',
+    version='2.1',
     description='Editor for Arturia MicroBrute',
     author='David García Goñi',
     author_email='dagargo@gmail.com',
